42d77d7b61027d0f7ab2176e8871294ee26d397c
Phil Cockfield <phil@cockfield.net>
2015-09-19 11:28:59 +1200

    Increase timeout to 20s.

--- test/test.js
+++ test/test.js
@@ -14,7 +14,7 @@ let modules = [
 
 
 describe("Modules:", function() {
-  this.timeout(10000);
+  this.timeout(20000);
   modules.forEach((name) => {
     it(name, (done) => { helpers.runTests(name, done) });
   });
